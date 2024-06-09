86b7a28cdc6e53bf9b465a7ee3ac93f60d1d7e6c
dsw54755 <168067092+dsw54755@users.noreply.github.com>
2024-06-09 16:39:21 +0200

    Task3
    Task3

--- task3_save_json.py
+++ task3_save_json.py
@@ -6,6 +6,6 @@ def save_json(data, file_path):
 
 if __name__ == "__main__":
     data = {"key": "value"}
-    output_file = "path/to/output.json"
+    output_file = "C:\\Users\\Asus-pc\\Desktop\\Projekt\\output.json"
     save_json(data, output_file)
     print(f"Zapisano do {output_file}")
