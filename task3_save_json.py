2620c6e4c90b73782e3da5f30ef74a7f592106e4
dsw54755 <168067092+dsw54755@users.noreply.github.com>
2024-06-09 16:32:46 +0200

    Task3
    Task3

--- task3_save_json.py
+++ task3_save_json.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     data = {"key": "value"}
     output_file = "path/to/output.json"
     save_json(data, output_file)
-    print(f"Data saved to {output_file}")
+    print(f"Zapisano do {output_file}")
