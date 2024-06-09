85577bba9c4f3c0700540172c0d1ba5350729d96
dsw54755 <168067092+dsw54755@users.noreply.github.com>
2024-06-09 16:34:38 +0200

    Task1
    Task1

--- task1_parse_args.py
+++ task1_parse_args.py
@@ -3,7 +3,7 @@ import os
 
 def parse_args():
     if len(sys.argv) != 3:
-        print("Usage: program.exe pathFile1.x pathFile2.y")
+        print("Użycie: program.exe pathFile1.x pathFile2.y")
         sys.exit(1)
     
     input_file = sys.argv[1]
@@ -13,15 +13,15 @@ def parse_args():
     output_ext = os.path.splitext(output_file)[1].lower()
 
     if input_ext not in ['.xml', '.json', '.yml', '.yaml']:
-        print("Unsupported input file format.")
+        print("zły format pliku.")
         sys.exit(1)
     
     if output_ext not in ['.xml', '.json', '.yml', '.yaml']:
-        print("Unsupported output file format.")
+        print("zły format pliku.")
         sys.exit(1)
     
     return input_file, output_file, input_ext, output_ext
 
 if __name__ == "__main__":
     input_file, output_file, input_ext, output_ext = parse_args()
-    print(f"Input file: {input_file}, Output file: {output_file}")
+    print(f"plik wejściowy: {input_file}, plik wyjściowy: {output_file}")
